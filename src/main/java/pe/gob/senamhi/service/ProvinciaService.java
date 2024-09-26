c14e77414172dc8e8e4607578505c1245b5483f5
JhuniorSalazarFer <jhuniorsalazarfer@outlook.com>
2024-09-26 11:27:45 -0500

    prov

--- src/main/java/pe/gob/senamhi/service/ProvinciaService.java
+++ src/main/java/pe/gob/senamhi/service/ProvinciaService.java
@@ -1,15 +1,14 @@
 package pe.gob.senamhi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import pe.gob.senamhi.dto.DepartamentoDto;
+import org.springframework.stereotype.Service;
 import pe.gob.senamhi.dto.ProvinciaDto;
 import pe.gob.senamhi.mapper.ProvinciaMapperService;
-import pe.gob.senamhi.repository.DepartamentoRepository;
 import pe.gob.senamhi.repository.ProvinciaRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class ProvinciaService extends ProvinciaMapperService {
     @Autowired
     private ProvinciaRepository repository;
